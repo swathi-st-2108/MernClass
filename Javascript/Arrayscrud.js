7c34a05bbafd3468275e9f57933dfa9c0e7aaad1
swathi-st-2108 <swathist2005@gmail.com>
2025-05-08 19:35:09 +0530

    login.html

--- Javascript/Arrayscrud.js
+++ Javascript/Arrayscrud.js
@@ -1,17 +1,68 @@
-let Travelsname= new Array("jolly","sky","wonder");
+let Travelsname = new Array("GT", "jolly", "SKY", "Wonder");
 
-let Adding=()=>{
-    let newvalue=document.getElementById("Travels").value;
+
+let Adding = () => {
+    let newvalue = document.getElementById('Travels').value;
+
     Travelsname.push(newvalue);
-    alert(newvalue +" added to the array.");
+
+    alert(newvalue + " this travels is added successfully..!")
     Printall()
 }
 
-var Printall=()=>{
-    let temp=""
+var Printall = () => {
+    let temp = " "
 
-    Travelsname.map((getting)=>{
-        temp+="<tr><td><ul><li>"+getting+"</li></ul></td></tr>";
-})
-    document.getElementById("List").innerHTML=<table><thead bgcolor='red'><tr><th>TravelsList</th></tr></thead><tbody bgcolor='pink'></tbody></table>;
+    Travelsname.map((getting) =>
+    {
+        temp += " <tr><td><ul><li>" + getting + "</li></ul></td></tr>";
+    })
+
+    document.getElementById('list').innerHTML = "<table><thead bgcolor='red'><tr><th>Travels List</th></tr></thead><tbody bgcolor='pink' align='center'>" + temp + "</tbody></table>";
+}
+
+let Updating = () => {
+    let updateindex = document.getElementById('Travels').value;
+
+    let newvalue = prompt(" please enter your update Travel name" +Travelsname[updateindex])
+
+    Travelsname[updateindex] = newvalue;  
+    Printall();
+}
+
+
+var Removing = () => {
+    let delete_value = document.getElementById('Travels').value;
+
+    Travelsname=Travelsname.filter((getting)=>
+    {
+        if(getting!==delete_value)
+        {
+            return getting;
+        }
+    })
+
+    Travelsname.map((get) => {
+        console.log(get)
+    });
+
+    Printall();
+}
+
+
+function Finding() {
+    let searchingvalue = document.getElementById('Travels').value;
+
+    for (i = 0; i < Travelsname.length; i++) {
+        if (Travelsname[i] === searchingvalue) {
+            return i;
+        }
+    }
+}
+
+function Findfirst() {
+    for (i = 0; i < Travelsname.length; i++) {
+        alert(" Travels names is :" + Travelsname[i]);
+        break;
+    }
 }
